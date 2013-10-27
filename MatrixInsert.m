b3c67453907d44a13bed463f279045c0d2b900b7
j326kim <jeremykim000@gmail.com>
2013-10-26 22:10:56 -0400

    Matrix Local to Global
    First Copy

---
+++ MatrixInsert.m
@@ -0,0 +1,22 @@
+function [G_K] = MatrixInsert(Global,angle,kValue,Node1,Node2)
+
+%   Detailed explanation goes here
+
+%create local matrix
+    a=floor(cos(angle)*10^10)/10^10;
+    b=floor(sin(angle)*10^10)/10^10;
+    
+    temp=[a^2 a*b;a*b b^2];
+    local=kValue*[temp -temp; -temp temp];
+    
+    [r,c]=size(local);
+    Node1=Node1*2-1;
+    Node2=Node2*2-1;
+    Global(Node1:Node1+r-1,Node1:Node1+c-1)= local;
+    Global(Node2:Node2+r-1,Node2:Node2+c-1)= local;
+    Global(Node2:Node2+r-1,Node1:Node1+c-1)= -local;
+    Global(Node1:Node1+r-1,Node2:Node2+c-1)= -local;
+    G_K=Global;
+
+end
+
