6a2acae7cb71da6e43c6197fd37fd9939e68e083
Daniel <jeremykim000@gmail.com>
2013-10-26 22:17:49 -0400

    Test
    Test

--- MatrixInsert.m
+++ MatrixInsert.m
@@ -2,13 +2,15 @@ function [G_K] = MatrixInsert(Global,angle,kValue,Node1,Node2)
 
 %   Detailed explanation goes here
 
-%create local matrix
+%   test
+%   create local matrix
     a=floor(cos(angle)*10^10)/10^10;
     b=floor(sin(angle)*10^10)/10^10;
     
     temp=[a^2 a*b;a*b b^2];
     local=kValue*[temp -temp; -temp temp];
     
+%   take local matrix and insert 
     [r,c]=size(local);
     Node1=Node1*2-1;
     Node2=Node2*2-1;
