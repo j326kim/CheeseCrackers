970effb8caefdc86c0ed20f01881d963ee592a74
j326kim <jeremykim000@gmail.com>
2013-11-04 22:18:47 -0500

    Gauss Seidel Function - Jeremy Kim
    Added convergence test

--- seidelSolver.m
+++ seidelSolver.m
@@ -4,6 +4,7 @@
 function result = seidelSolver(A,B,iteration)
     loopVector = ones(size(B)); %Initial guess - can be anything ?
     loopVector(2,1) = 0;
+    iterationVector = loopVector; % Copy of loop vector (for convergence)
     for i=1:1:iteration % More iteration = better approximation
         for j=1:1:length(A) % Number of rows or columns of square A matrix
             sumInEachRow = 0;
@@ -18,6 +19,20 @@ function result = seidelSolver(A,B,iteration)
             %Update the loop vector for further calculation
             loopVector(j,1) = (B(j,1) - sumInEachRow)/A(j,j);
         end
+        %Convergence Test (Only to see if it is diverging or not
+        %for each iteration
+        maximumError = -99999999;
+        %Compare previous iteration vector with current iteration vector
+        for d=1:1:length(iterationVector)
+            error = (( loopVector(d,1) - iterationVector(d,1) ) ...
+                / loopVector(d,1)) * 100;
+            if (error > maximumError)
+                maximumError = error;
+            end
+        end
+        fprintf('Max Error for iteration %i: %f\n', i, maximumError);
+        %Make current copy, the previous copy vector
+        iterationVector = loopVector;
     end
     %transfer loopVector to result then return output of the function
     result = loopVector;
