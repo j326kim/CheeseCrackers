4c20fd3119aceef6b2d7e58a6d44a4199165cfb0
j326kim <jeremykim000@gmail.com>
2013-11-05 21:48:09 -0500

    Solution Script - Changes made by JK
    Placed basic structure for overall solution.

--- SolutionScript.m
+++ SolutionScript.m
@@ -9,3 +9,21 @@ dx = 0.0001;  %position step
 %force input vector
 Fap = 0:Fslope*dt:Flim;  %
 
+%Where calculation begins using Gauss Seidel
+U = zeros(6,1); %Change the hard-coded value to (length(Global),1))
+for i=1:1:length(Global)
+    %Global - Square, U - Initial Guess, Fap - Force Vector
+    updatedVector = seidelSolver(Global,U, Fap);
+    %Check if breakage occurs based on the displacement of every elements
+    %-for i=1:1:length(matrix with maxmimum allowable displacement for each element)
+    %DO Animation Here
+    %-Plot the displacement vector here
+    %Increment Force applied on center of bow
+    %Update Force Matrix - Calculate force applied on each element for next
+    %iteration
+    %Update Matrices of Constants, i.e C,K
+    %Get global matrix again with mass, stiffness, and damping functions
+end
+
+
+
