6fcc0770dd01f55f5dd842cdd65e0eba28c3971e
j326kim <jeremykim000@gmail.com>
2013-11-07 02:05:33 -0500

    VedantC- Update 2
    Sparce has been updated to fix only bow center node, solution is further
    developed.

--- Sparse.m
+++ Sparse.m
@@ -1,21 +1,18 @@
-function [G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F] = Sparse(G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F)
+function [G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F] = Sparse(G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F,centre)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     
-    for i=size(G_U,1):-1:1   
-        if G_U(i,1)==0
-            G_K=removerows(G_K,i); %remove row
-            G_K(:,i)=[]; %remove column
-            G_C=removerows(G_C,i);
-            G_C(:,i)=[];
-            G_M=removerows(G_M,i);
-            G_M(:,i)=[];
-            G_F=removerows(G_F,i);
-            G_U=removerows(G_U,i);
-            G_Ud=removerows(G_Ud,i);
-            G_Udd=removerows(G_Udd,i);
-        end
+    for i = 0:2
+        G_K=removerows(G_K,3*centre-i); %remove row
+        G_K(:,3*centre-i)=[]; %remove column
+        G_C=removerows(G_C,3*centre-i);
+        G_C(:,3*centre-i)=[];
+        G_M=removerows(G_M,i);
+        G_M(:,3*centre-i)=[];
+        G_F=removerows(G_F,3*centre-i);
+        G_U=removerows(G_U,3*centre-i);
+        G_Ud=removerows(G_Ud,3*centre-i);
+        G_Udd=removerows(G_Udd,3*centre-i);
     end
-    
 end
 
