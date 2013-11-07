60819103dd89dc7a82590c024389ca5c7b6274a1
j326kim <jeremykim000@gmail.com>
2013-11-07 04:17:10 -0500

    Update Animation.m
    Changed structure

--- Animation.m
+++ Animation.m
@@ -1,23 +1,34 @@
-close all;
-f = figure('color','white');
-title('Bow and Arrow!','Color',[.6 0 0])
-axis off,axis equal
-ylim([-1000 1000]);
-xlim([-1000 1000])
-%-----makes the target-----%
-c = -pi:.04:pi;
-cx = 100*cos(c)-800;
-cy = -100*sin(c);
-target=line(cx, cy, 'color', [.4 .4 .8],'LineWidth',1);
-hold on
-%--------------------------%
-
-bow=zeros(size(Element,1),1);
-for i=1:size(Element,1)
-    bow(i,1)=line(Element(1,1:2),Element(1,3:4),'color','blue','LineWidth',x);
+function [] = Animation()
+    
+    setsOfPositionVectors = rand(93,93); %Take this hard code out
+    
+    figure('color','white');
+    title('Bow and Arrow!','Color',[.6 0 0])
+    axis off,axis equal
+    ylim([-1000 1000]);
+    xlim([-1000 1000])
+    
+    numOfLoop = length(setsOfPositionVectors);
+    for j=1:1:numOfLoop
+        x;
+        y;
+        xx = horzcat
+        positionVector = setsOfPositionVectors(:,j);
+        %for i=1:1:size(positionVector,1)-2
+        for i=1:1:length(positionVector)
+            n = 3;
+            x = positionVector(1 : n : end);
+            y = positionVector(2 : n : end);
+            %plot(x,y,'color','blue','LineWidth',4);
+            %hold on
+            %drawnow 
+        end
+    end
 end
 
 
+%x=[positionVector(3*i-2) positionVector(3*(i+1)-2)];
+%y=[positionVector(3*i-1) positionVector(3*(i+1)-1)];
 
 
 
