e170bf99cbadf98408bc9a09f6aa167b376a4ba1
Vivek <jeremykim000@gmail.com>
2013-11-07 04:08:57 -0500

    returning my penis
    KeffMatrises(i,:,:) defined, i = number of elements

--- CoefMaker.m
+++ CoefMaker.m
@@ -3,6 +3,7 @@ StiffnessGlobal = zeros(3*(length(xfinal)+1));
 DampingGlobal = zeros(3*(length(xfinal)+1));
 G_K = zeros(3*(length(xfinal)+1));
 G_C = zeros(3*(length(xfinal)+1));
+KeffMatrises = zeros(length(xfinal), 6, 6);
 
 for i = 2:(length(xfinal))
     N1 = i-1;
@@ -12,8 +13,8 @@ for i = 2:(length(xfinal))
     Angle = angles(i);
     I = w*thickness(i)^3/12; 
         
-    G_K = G_K + MatInsert(StiffnessGlobal,Angle,E,L,A,I,N1,N2);
-    G_C = G_C + dampInsert(DampingGlobal,Angle,L,A,N1,N2);
+    [G_K, KeffMatrises(i,:,:)] = MatInsert(G_K,Angle,E,L,A,I,N1,N2);
+    [G_C ] = dampInsert(G_C,Angle,L,A,N1,N2);
     
 end
 
@@ -24,7 +25,7 @@ A = pi*stringR^2;
 N1 = 1; 
 N2 = length(xfinal)+1;
 
-Angle = 
+% Angle = 
 
 
 
