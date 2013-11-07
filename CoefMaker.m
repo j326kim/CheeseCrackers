c7689dc09887a29f43f8b203d4ab56fb0e51f05f
Daniel <jeremykim000@gmail.com>
2013-11-07 04:33:52 -0500

    Updated coef maker with mass matrix maker

--- CoefMaker.m
+++ CoefMaker.m
@@ -1,9 +1,8 @@
 E = xlsread('InputFile', 1, 'B2');
-StiffnessGlobal = zeros(3*(length(xfinal)+1));
-DampingGlobal = zeros(3*(length(xfinal)+1));
 G_K = zeros(3*(length(xfinal)+1));
 G_C = zeros(3*(length(xfinal)+1));
-KeffMatrises = zeros(length(xfinal), 6, 6);
+G_M = zeros(3*(length(xfinal)+1));
+KeffMatrises = zeros(6, 6,length(xfinal)); %KeffMatrises(:,:,i)
 
 for i = 2:(length(xfinal))
     N1 = i-1;
@@ -13,19 +12,12 @@ for i = 2:(length(xfinal))
     Angle = angles(i);
     I = w*thickness(i)^3/12; 
         
-    [G_K, KeffMatrises(i,:,:)] = MatInsert(G_K,Angle,E,L,A,I,N1,N2);
-    [G_C ] = dampInsert(G_C,Angle,L,A,N1,N2);
+    [G_K, KeffMatrises(:,:,i)] = MatInsert(G_K,Angle,E,L,A,I,N1,N2);
+    G_C = dampInsert(G_C,Angle,L,A,N1,N2);
+    G_M = DistributedMassMatrixMaker(G_M,N1,N2,p,A,L);
     
 end
 
-L = stringL/2;
-E = stringE;
-A = pi*stringR^2;
-
-N1 = 1; 
-N2 = length(xfinal)+1;
-
-% Angle = 
 
 
 
