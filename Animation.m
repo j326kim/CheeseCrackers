1aac365d4c1fc33f78aea92c4a1253aeefc27818
j326kim <jeremykim000@gmail.com>
2013-11-07 05:12:13 -0500

    Update Animation.m
    JK

--- Animation.m
+++ Animation.m
@@ -1,34 +1,23 @@
 function [] = Animation()
     
-    setsOfPositionVectors = rand(93,93); %Take this hard code out
-    
+    setOfFinalX = rand(93,1000);
+    setOfFinalY = rand(93,1000);
+
     figure('color','white');
     title('Bow and Arrow!','Color',[.6 0 0])
     axis off,axis equal
     ylim([-1000 1000]);
     xlim([-1000 1000])
     
-    numOfLoop = length(setsOfPositionVectors);
+    numOfLoop = length(setOfFinalX);
     for j=1:1:numOfLoop
-        x;
-        y;
-        xx = horzcat
-        positionVector = setsOfPositionVectors(:,j);
-        %for i=1:1:size(positionVector,1)-2
-        for i=1:1:length(positionVector)
-            n = 3;
-            x = positionVector(1 : n : end);
-            y = positionVector(2 : n : end);
-            %plot(x,y,'color','blue','LineWidth',4);
-            %hold on
-            %drawnow 
-        end
+        vectorX = setOfFinalX(:,j);
+        vectorY = setOfFinalY(:,j);
+        pause(0.1);
+        figure(1)
+        plot(vectorX,vectorY,'color','blue','LineWidth',4);
+        drawnow 
     end
 end
-
-
-%x=[positionVector(3*i-2) positionVector(3*(i+1)-2)];
-%y=[positionVector(3*i-1) positionVector(3*(i+1)-1)];
-
 
 
