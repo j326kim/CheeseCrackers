98d57533ebde2524acf4e4c08489033f88e71e0c
Daniel <jeremykim000@gmail.com>
2013-11-07 04:37:40 -0500

    corrected spelling mistake

--- CoefMaker.m
+++ CoefMaker.m
@@ -2,7 +2,7 @@ E = xlsread('InputFile', 1, 'B2');
 G_K = zeros(3*(length(xfinal)+1));
 G_C = zeros(3*(length(xfinal)+1));
 G_M = zeros(3*(length(xfinal)+1));
-KeffMatrises = zeros(6, 6,length(xfinal)); %KeffMatrises(:,:,i)
+KeffMatrices = zeros(6, 6,length(xfinal)); %KeffMatrises(:,:,i)
 
 for i = 2:(length(xfinal))
     N1 = i-1;
@@ -12,10 +12,9 @@ for i = 2:(length(xfinal))
     Angle = angles(i);
     I = w*thickness(i)^3/12; 
         
-    [G_K, KeffMatrises(:,:,i)] = MatInsert(G_K,Angle,E,L,A,I,N1,N2);
+    [G_K, KeffMatrices(:,:,i)] = MatInsert(G_K,Angle,E,L,A,I,N1,N2);
     G_C = dampInsert(G_C,Angle,L,A,N1,N2);
     G_M = DistributedMassMatrixMaker(G_M,N1,N2,p,A,L);
-    
 end
 
 
