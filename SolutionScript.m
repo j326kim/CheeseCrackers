4a97b835e166c8c57002c4fde09c0d692a2e40eb
j326kim <jeremykim000@gmail.com>
2013-11-07 16:08:06 -0500

    Update
    I changed some values in the coeff shit to just take shit from the excel
    sheet

--- SolutionScript.m
+++ SolutionScript.m
@@ -6,13 +6,13 @@ Flim = 10000; %Limit on force vector; biiigggg value to set
                  %limit outside of possible failure point
 Fslope = 200; %ROC of the force increments; calculated based on
               %typical loading at 1s, or full draw in regular use
-dt = 0.001; %time step
+dt = 0.00001; %time step
 p = xlsread('InputFile', 1, 'B4'); %Density (wood)
 w = xlsread('InputFile', 1, 'B6'); %Width of wood (Constant)
 stringMass = xlsread('InputFile', 1, 'H4'); % Mass of the string 
 stringE = xlsread('InputFile', 1, 'H2'); %String modulus of elasticity
 stringL = xlsread('InputFile', 1, 'H6'); %String length
-stringR = xlsread('InputFile', 1, 'H8'); %String Radius
+stringD = xlsread('InputFile', 1, 'H8'); %String Radius
 failure = 0; %Failure flag; if failure condition is reached, set to 1
 count = 0; %Number of iterations the while loop has gone through.
 
@@ -29,7 +29,7 @@ YAni = zeros(length(xfinal(1,:)+1), Flim); %Y animation matrix
 %force input vector
 Fap = 0:Fslope*dt:Flim; 
 
-while failure == 0 || Fap(1,count+1) ~= Flim
+while failure == 0 && Fap(1,count+1) ~= Flim
     % Making Global matrices
     if count > 0
         for i = 1:length(xfinal(1,:))
@@ -47,7 +47,7 @@ while failure == 0 || Fap(1,count+1) ~= Flim
     
     ElementVec; % Makes a vector, elementVec(), with element lengths.
     CoefMaker; 
-    Sparse(G_K,G_C,G_M,U,Uplus,Uminus,F,indexcenternode);
+    [G_K,G_C,G_M,U,Uplus,Uminus,F]= Sparse(G_K,G_C,G_M,U,Uplus,Uminus,F,indexcenternode);
     
 %     %Setup to make everything in the form Ax = B
 %     A = 1/dt^2*G_M + 1/(2*dt)*G_C; 
@@ -57,10 +57,10 @@ while failure == 0 || Fap(1,count+1) ~= Flim
 %     %make force vector
 %     B =  G1 + G2 + F;
 %     
-%     %Solve for Uplus
-%     Uplus = seidelSolver(G_M,G_K,G_C,Uminus,U,dt,F);
-%     Uminus = U;
-%     U = Uplus; 
+    %Solve for Uplus
+    Uplus = seidelSolver(G_M,G_K,G_C,Uminus,U,dt,F);
+    Uminus = U;
+    U = Uplus; 
 %     
 %     %Check for failure
 %     failure = IsBroken;
