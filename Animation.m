4056a4fdda5df9cc1db32507921f82c7871bd59e
Daniel <jeremykim000@gmail.com>
2013-11-08 05:41:41 -0500

    used line instead of plot

--- Animation.m
+++ Animation.m
@@ -1,22 +1,33 @@
 function [] = Animation(setOfFinalXBow,setOfFinalYBow, ...
 setOfFinalXString, setOfFinalYString)
 
-    figure('color','white');
+    figure(1);
     title('Bow and Arrow!','Color',[.6 0 0])
     axis off,axis equal
-    ylim([-1000 1000]);
-    xlim([-1000 1000])
+    ylim([-1 1]);
+    xlim([-1 1])
     
-    numOfLoop = length(setOfFinalXBow);
-    for j=1:1:numOfLoop
-        vectorXBow = setOfFinalXBow(:,j);
-        vectorYBow = setOfFinalYBow(:,j);
-        vectorXString = setOfFinalXString(:,j);
-        vectorYString = setOfFinalYString(:,j);
-        pause(0.1);
-        figure(1)
-        plot(vectorXBow,vectorYBow,'color','blue','LineWidth',4);
-        plot(vectorXString,vectorYString,'color','red','LineWidth',2);
-        drawnow 
+%     numOfLoop = length(setOfFinalXBow);
+    
+    for i=1:(length(setOfFinalXBow)-1)
+        bow(i,1)=line(setOfFinalXBow(1,i:i+1),setOfFinalYBow(1,i:i+1),'color','blue','LineWidth',3);
+        if i<3
+            string(i,1)=line(setOfFinalXString(1,i:i+1),setOfFinalYString(1,i:i+1),'color','red','LineWidth',4);
+        end
     end
+% 
+%     for j=1:1:numOfLoop
+%         vectorXBow = setOfFinalXBow(:,j);
+%         vectorYBow = setOfFinalYBow(:,j);
+%         if j<4
+%             vectorXString = setOfFinalXString(:,j);
+%             vectorYString = setOfFinalYString(:,j);
+%         end
+%         pause(0.1);
+%         figure(1)
+%         plot(vectorXBow,vectorYBow,'color','blue','LineWidth',4);
+%         plot(vectorXString,vectorYString,'color','red','LineWidth',2);
+%         drawnow 
+%         hold on
+%     end
 end
